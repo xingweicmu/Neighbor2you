8bbb87bbffbb9f3394e5a6da1ae1b376f1fdef62
VennoFang <octtty@gmail.com>
2015-04-14 10:07:12 -0700

    refine list items xml

--- app/src/main/java/com/cmu/neighbor2you/adapter/MainPageListViewAdapter.java
+++ app/src/main/java/com/cmu/neighbor2you/adapter/MainPageListViewAdapter.java
@@ -49,16 +49,17 @@ public class MainPageListViewAdapter extends BaseAdapter {
             vi = inflater.inflate(R.layout.list_row, null);
 
         TextView title = (TextView)vi.findViewById(R.id.title); // title
-        TextView artist = (TextView)vi.findViewById(R.id.artist); // artist name
+        TextView poster = (TextView)vi.findViewById(R.id.poster); // artist name
         TextView duration = (TextView)vi.findViewById(R.id.duration); // duration
         ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image); // thumb image
-
+        TextView distance = (TextView)vi.findViewById(R.id.distance);
         Request item = data.get(position);
 
         // Setting all values in listview
         title.setText(item.getItemName());
-        artist.setText(item.getRequester());
+        poster.setText("Posted by " + item.getRequester());
         duration.setText(String.valueOf(item.getDeadline()));
+        distance.setText(String.valueOf(item.getDistance()));
       //  imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL), thumb_image);
         return vi;
     }
